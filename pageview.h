de5efd59968c7f6a399ed43f0e1a97571d1217db
billy <bingl42@foxmail.com>
2023-08-15 17:36:25 +0800

    edit pageview, wip

--- pageview.h
+++ pageview.h
@@ -7,29 +7,58 @@
 #include "pageitem.h"
 #include <poppler-qt5.h>
 
-
 class PageView : public QWidget {
     Q_OBJECT
   public:
     PageView(QWidget *parent = nullptr);
-    void load(Poppler::Document *docu){
-        for(int i = 0; i< docu->numPages(); i++){
+
+    //init
+    void load(Poppler::Document *docu) {
+        for (int i = 0; i < docu->numPages(); i++) {
             m_pages.push_back(docu->page(i));
         }
     }
+    void setViewSize(){}
+    QSize boardSize() const;
+
+
+    //scale
     void autoscale();
     void setScale(float incr);
-    QSize boardSize() const;
+    void scaleToPageWidth(){}
+    void scaleToPageHeight(){}
+    void scaleToPageFit(){}
+
     void displayInfo() const;
-    void next();
-    void prev();
+
+    void load_page(int n);
+    void update_image();
+
+    //word.
+    QStringList words_forCurPage();
+    void update_filter();
+    QStringList check_wordlevel(const QStringList &wordlist);
+
+    //highlight
+    Poppler::HighlightAnnotation *make_highlight(QRectF region);
+
+    //jump
+    void go_next();
+    void go_prev();
+    void go_to(int n);
+  signals:
+    // NOTE: directConnection signal-slot <=> cb on event.
+    void pageLoadBefore();
+    void PageLoadDone();
 
   private:
+    class Private;
+    Private *d;
     QGraphicsScene *m_scene{nullptr};
     QGraphicsView *m_view{nullptr};
     PageItem *m_photoItem{nullptr};
 
-    QList<Poppler::Page*> m_pages; //TODO: optimi
+    QList<Poppler::Page *> m_pages;  // TODO: optimi
     QFileInfoList m_filelist{};
     int m_index{0};
     QPixmap m_pixmap;
